bc80a73606c65cf9416487e3b4a63262764f07d3
rafaelzeredo <rafaelmartinszeredo@gmail.com>
2024-08-03 19:06:23 -0300

    Test functionality

--- script.js
+++ script.js
@@ -21,22 +21,22 @@ function handleFormSubmission(event) {
         message: message
     };
 
+    // Example of console logging form data
+    console.log(`Category: ${category}, Name: ${name}, Email: ${email}, Message: ${message}`);
+
     emailjs.send("service_category_message", "template_8agfk2i", emailParams)
         .then(function(response) {
             console.log('Email Sent!', response.status, response.text);
             alert('Your message has been sent sucessfully!');
-            document.querySelector('form').reset(); // Reset form after successfull submission
+    document.querySelector('form').reset(); // Reset form after successfull submission
         }, function(error) {
             console.error('Error sending email: ', error);
             alert('Oops! An error occurred while sending your message.');
         });
 
-    // Example of console logging form data
-    console.log(`Category: ${category}, Name: ${name}, Email: ${email}, Message: ${message}`);
-
     // Replace with your form submission logic or API call
     // For demonstration purposes, it logs the form data to the console
-    alert('Message sent! Check the console for form data.');
+    //alert('Message sent! Check the console for form data.');
 }
 
 // Event listener for form submission
